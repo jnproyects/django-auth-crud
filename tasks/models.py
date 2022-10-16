7a2f83bd0de9d26eddd3d6d55e75089b91bf2cbe
Juan M <judith1912black@gmail.com>
2022-10-16 16:40:17 -0500

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,16 @@
+from calendar import c
+from turtle import title
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 500, blank = True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_completed = models.DateTimeField(null = True, blank =True)
+    important = models.BooleanField(default = False)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' by ' + self.user.username
